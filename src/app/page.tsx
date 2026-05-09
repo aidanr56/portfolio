5ff53462d2dcb5cb621a9e0798ca4f98c5dcd7ea
Aidan <121110118@umail.ucc.ie>
2026-05-08 23:51:49 -0400

    overhaul of landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,22 +3,70 @@ import Image from "next/image";
 import { projects } from "@/data/projects";
 import ProjectCard from "@/components/ProjectCard";
 
+const logo_pictures = "w-35 h-35 object-cover"
+
 export default function Home() {
   return (
-    <main className="max-w-4xl mx-auto px-6 py-12">
-      <div className="mb-8">
-        <h1 className="text-4xl font-semibold tracking-tight text-zinc-900 mb-2">
+  <div className="flex min-h-screen">
+    <aside className="w-64 shrink-0 border-r border-gray-200 p-6 flex flex-col sticky top-0 h-screen">
+        <div className="flex flex-col items-center text-center mb-5">
+          <img
+            src="/profile_picture.jpg"
+            alt="Image of Aidan Roche"
+            className="w-45 h-45 rounded-full object-cover"
+          />
+        </div>
+        <h1 className="text-4xl font-semibold tracking-tight text-zinc-900">
           Aidan Roche
         </h1>
+        <div className="border-t border-sinz-100 w-16 my-2"/>
         <p className="text-xl text-zinc-600 mb-2">
           Psychology and Computer Science Graudate
         </p>
 
-        <div className="border-t border-sinz-100 my-2 w-16"/>
+    </aside>
 
-        <p className="text-lg text-zinc-800 leading-relaxed max-w-3xl mt-4">
-          I am a young professional, with a unique multi-disciplinary background in AI/ML, Computer Engineering, and Human Factors. I am looking to take the next step in my career, and am excited by the challenge of developing and implementing technology with a genuine impact. I have experience in Machine Translation, Human Factors, and AI implementation, and excel at bridging the gap between people and technology.
+    <main className="max-w-4xl mx-auto px-6 py-6">
+      <div className="flex flex-col items-center mb-6">
+        <p className="font-mono text-center text-2xl text-zinc-800 leading-relaxed max-w-3xl mt-4">
+          Young developer with unique experience.
         </p>
+        <div className="w-50 h-px bg-gray-600 mt-4" />
+      </div>
+
+      <div className="flex justify-center items-center gap-10 text-center text-2xl text-cyan-700 my-6">
+        <div className="text-center">
+          <p>Artficial<br />Intelligence</p>
+        </div>
+        <div className="w-3 h-3 rounded-full bg-gray-700 shrink-0" />
+        <div className="text-center">
+          <p>Applied<br />Psychology</p>
+        </div>
+        <div className="w-3 h-3 rounded-full bg-gray-700 shrink-0" />
+        <div className="text-center">
+          <p>Human Factors<br />Engineering</p>
+        </div>
+      </div>
+
+      <div className="flex justify-center gap-15 mb-6">
+        <div className="flex flex-col items-center text-center">
+          <img
+            src="/ucc_crest.jpg"
+            alt="Image of Aidan Roche"
+            className={logo_pictures}
+          />
+          <p className="text-zinc-1000">University College Cork</p>
+          <p className="text-xl text-zinc-600">Bachelor's Degree (Honors)</p>
+        </div>
+        <div className="flex flex-col items-center text-center">
+          <img
+            src="/Johnson_Controls.png"
+            alt="Image of Aidan Roche"
+            className={logo_pictures}
+          />
+          <p className="text-zinc-1000">Johnson Controls</p>
+          <p className="text-xl text-zinc-600">AI/ML Technology Intern</p>
+        </div>
       </div>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 mb-8">
@@ -85,5 +133,6 @@ export default function Home() {
       </div>
 
     </main>
+  </div>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,22 @@
 import Link from "next/link";
 
+const link_text = "text-lg text-zinc-600 hover:text-zinc-900 transition-colors"
+
 export default function Navbar() {
   return (
     <nav className="border-b border-zinc-200 bg-white">
       <div className="max-w-3xl mx-auto px-6 py-4 flex items-center justify-between">
-        <Link href="/" className="text-zinc-900 font-semibold tracking-tight">
+        <Link href="/" className="text-xl text-zinc-900 font-semibold tracking-tight">
           Home
         </Link>
         <div className="flex gap-6">
-          <Link href="/projects" className="text-zinc-600 hover:text-zinc-900 transition-colors">
+          <Link href="/projects" className={link_text}>
             Projects
           </Link>
-          <Link href="/papers" className="text-zinc-600 hover:text-zinc-900 transition-colors">
+          <Link href="/papers" className={link_text}>
             Papers
           </Link>
-          <Link href="/about" className="text-zinc-600 hover:text-zinc-900 transition-colors">
+          <Link href="/about" className={link_text}>
             About
           </Link>
         </div>
